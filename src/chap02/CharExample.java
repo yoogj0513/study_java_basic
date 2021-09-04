72c5558dc68d5e4e129a40c9f32be7006382f89a
yoogj <yoogj0513@naver.com>
2021-09-04 18:06:27 +0900

    [2021-09-04] byte, char 타입

---
+++ src/chap02/CharExample.java
@@ -0,0 +1,19 @@
+package chap02;
+
+public class CharExample {
+
+	/* 
+	 * char 타입 변수
+	 * int에 char 타입 변수를 저장하면 유니코드 얻을 수 있
+	 */
+	public static void main(String[] args) {
+		char c1 = 'A';			// 문자를 직접 저장 
+		char c2 = 65;			// 10진수로 저장 
+		char c3 = '\u0041';		// 16진수로 저장
+		
+		int uniCode = c1; 		// 유니코드 얻기
+		
+		
+	}
+	
+}
